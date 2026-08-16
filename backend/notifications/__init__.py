c0e4177b62c513969427b7cac92f1b700b3c16c0
evidencealexis-tech-johunter-pro <evidencealexis@gmail.com>
2026-08-16 23:26:41 +0100

    refactor: add notification domain package

---
+++ backend/notifications/__init__.py
@@ -0,0 +1,13 @@
+from .service import (
+    add_notification,
+    clear_notifications_for_user,
+    list_notifications_for_user,
+    mark_all_notifications_read,
+)
+
+__all__ = [
+    "add_notification",
+    "clear_notifications_for_user",
+    "list_notifications_for_user",
+    "mark_all_notifications_read",
+]
